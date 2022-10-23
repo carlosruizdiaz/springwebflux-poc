5c47327d8c086c5be7207df7261db5fcd0e36c4d
Carlos A. Ruiz Diaz <ruizdiaz.ca@gmail.com>
2022-10-23 12:52:09 -0300

    Inicializacion del proyecto

---
+++ src/test/java/com/reactive/programming/poc/PocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactive.programming.poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
